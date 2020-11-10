aab5884794ad3ae724d74d91d2a8f57a614a8826
edm20627 <eng00627@gmail.com>
2020-11-10 16:35:40 +0900

    docker 修正

--- docker/mysql/initdb.d/create_db_table.sql
+++ docker/mysql/initdb.d/create_db_table.sql
@@ -1,3 +1,15 @@
 SET CHARSET UTF8;
+
 DROP DATABASE IF EXISTS go_sample;
-CREATE DATABASE IF NOT EXISTS go_sample DEFAULT CHARACTER SET utf8;
+
+DROP DATABASE IF EXISTS development;
+CREATE DATABASE IF NOT EXISTS development DEFAULT CHARACTER SET utf8;
+
+DROP TABLE IF EXISTS lists;
+
+CREATE TABLE lists (
+  id   integer primary key auto_increment,
+  text text,
+  created_at timestamp not null DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp not null DEFAULT CURRENT_TIMESTAMP
+);
